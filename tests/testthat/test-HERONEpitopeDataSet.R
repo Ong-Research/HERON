a16941724b8a1ee3260a8b4ce3294ccd1b759bed
mcilwain <sean.mcilwain@wisc.edu>
2023-06-01 09:48:45 -0500

    Small change to test

--- tests/testthat/test-HERONEpitopeDataSet.R
+++ tests/testthat/test-HERONEpitopeDataSet.R
@@ -3,5 +3,5 @@ test_that("HERONEpitopeDataSet works", {
     pvalues <- matrix(1, nrow=2, ncol=2)
 
     expect_true(validObject(.HERONEpitopeDataSet()))
-    expect_true(validObject(HERONEpitopeDataSet(pvalues = pvalues)))
+    expect_true(validObject(HERONEpitopeDataSet(pvalue = pvalues)))
 })
