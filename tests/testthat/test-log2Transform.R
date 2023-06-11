7f153d6e3733dd5b4516960b8a56caf547f7d992
mcilwain <sean.mcilwain@wisc.edu>
2023-06-11 17:04:28 -0500

    Small comments

--- tests/testthat/test-log2Transform.R
+++ tests/testthat/test-log2Transform.R
@@ -1,8 +1,11 @@
 test_that("log2Transform works", {
     data("heffron2021_wuhan")
+
+    #Test bad arguments
     expect_error(log2Transform(assay(heffron2021_wuhan,"exprs")))
     expect_error(log2Transform(.HERONEpitopeDataSet()))
 
+    #Test result
     test_se <- heffron2021_wuhan
     assay(test_se, "exprs") <- 2^assay(test_se, "exprs")
 
